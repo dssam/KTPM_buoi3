11b96a3fc1523294084769d7831c2e5452d9d534
Sam <1551010110sam@gmail.com>
2017-10-03 14:34:15 +0700

    Thay doi

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,101 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbSoa = new System.Windows.Forms.TextBox();
+            this.tbSob = new System.Windows.Forms.TextBox();
+            this.tbKQ = new System.Windows.Forms.TextBox();
+            this.btCong = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "số a";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(27, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "số b";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(23, 124);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Kết quả";
+            // 
+            // tbSoa
+            // 
+            this.tbSoa.Location = new System.Drawing.Point(98, 22);
+            this.tbSoa.Name = "tbSoa";
+            this.tbSoa.Size = new System.Drawing.Size(140, 20);
+            this.tbSoa.TabIndex = 1;
+            // 
+            // tbSob
+            // 
+            this.tbSob.Location = new System.Drawing.Point(98, 66);
+            this.tbSob.Name = "tbSob";
+            this.tbSob.Size = new System.Drawing.Size(140, 20);
+            this.tbSob.TabIndex = 1;
+            // 
+            // tbKQ
+            // 
+            this.tbKQ.Location = new System.Drawing.Point(98, 117);
+            this.tbKQ.Name = "tbKQ";
+            this.tbKQ.Size = new System.Drawing.Size(140, 20);
+            this.tbKQ.TabIndex = 1;
+            // 
+            // btCong
+            // 
+            this.btCong.Location = new System.Drawing.Point(138, 186);
+            this.btCong.Name = "btCong";
+            this.btCong.Size = new System.Drawing.Size(99, 33);
+            this.btCong.TabIndex = 2;
+            this.btCong.Text = "Cộng";
+            this.btCong.UseVisualStyleBackColor = true;
+            this.btCong.Click += new System.EventHandler(this.btCong_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.btCong);
+            this.Controls.Add(this.tbKQ);
+            this.Controls.Add(this.tbSob);
+            this.Controls.Add(this.tbSoa);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbSoa;
+        private System.Windows.Forms.TextBox tbSob;
+        private System.Windows.Forms.TextBox tbKQ;
+        private System.Windows.Forms.Button btCong;
     }
 }
 
